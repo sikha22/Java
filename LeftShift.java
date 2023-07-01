ab95a1432ab712221eab78f977ae772f600e3ead
Sikha Kumari <84958102+sikha22@users.noreply.github.com>
2023-07-01 13:32:23 +0530

    Add files via upload

---
+++ LeftShift.java
@@ -0,0 +1,8 @@
+public class LeftShift {
+    public static void main(String[] args){
+        int i = 8; // 1000
+        int y = i << 2; //100000
+        int z = i >> 2; // 10
+        System.out.println(y+" "+z);
+    }
+}
